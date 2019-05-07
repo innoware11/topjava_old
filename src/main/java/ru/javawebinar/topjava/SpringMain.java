f4ddc2479bf4640666f2bb5110d19d2b962df9c6
innoware11 <innoware11@gmail.com>
2019-05-07 09:30:27 +0300

    Add Spring context and SpringMain

---
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.topjava;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.mock.MockUserRepository;
+
+import java.util.Arrays;
+
+public class SpringMain {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        System.out.println(Arrays.toString(appCtx.getBeanDefinitionNames()));
+        MockUserRepository mockUserRepository = (MockUserRepository) appCtx.getBean("MockUserRepository");
+        mockUserRepository = appCtx.getBean(MockUserRepository.class);
+        appCtx.close();
+    }
+}
